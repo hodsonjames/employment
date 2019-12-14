36433d139d0556d592825f6c34cf3df62cb5a005
Steven Deng <hongdeng@berkeley.edu>
2019-12-14 14:04:13 -0800

    Added the folder for company ranking script

---
+++ company_rank/final.py
@@ -0,0 +1,105 @@
+from py2neo import Graph, Node, Relationship
+import pandas as pd
+import os
+
+# manually set up these on the server before running the script
+uri = "bolt://localhost:7687"
+user = "neo4j"
+password = "123456"
+
+
+# initialize the graph object
+g = Graph(uri=uri, user=user, password=password)
+
+# prepare paths to desired datasets
+dataset_names = [x for x in os.listdir('../cognism-enriched/') if 'done' not in x]
+
+
+
+# process one year of data and produce a page rank by iterating through all datasets for one year
+def process_one_year(year, batch_size, output_path):
+	# load the yearly graph
+	for dataset in dataset_names:
+		# absolute path of the dataset
+		file_path = 'file:///DATA-0/fedyk/cognism-enriched/' + dataset
+		# loading & graph building 
+		query = """
+				CALL apoc.load.json({}) YIELD value 
+				WITH value.user_id AS Username, value.elite_edu AS Elite, value.start AS SDate, value.end AS EDate, value.org AS Company, value.is_edu AS EduFlag
+				WHERE SDate CONTAINS {} AND EduFlag CONTAINS "False" AND Company IS NOT NULL
+				MERGE(e:Employee {{eid: Username, elite: Elite, sdate: SDate, edate:EDate, company:Company}})
+				MERGE(c:Company {{name: Company, eliteNumber: 0, totalNumber: 0}})
+				MERGE (e)-[r:WORKSFOR]->(c)
+			""".format(str(batch_size), '\'' + file_path + '\'', '\'' + str(year) + '\'')
+		g.run(query)
+
+		# calculate number of elite employees in each company in year
+		query = """
+				MATCH (e1:Employee {elite: "True"})-->(c1:Company)
+				SET c1.eliteNumber = c1.eliteNumber + 1
+			"""
+
+		g.run(query)
+
+		# calculate total number of employees in each company in year
+		query = """
+				MATCH (e1:Employee)-->(c1:Company)
+				SET c1.totalNumber= c1.totalNumber + 1
+			"""
+
+		g.run(query)
+
+		# if an egde already exits, will this set the weight of already existing edge to 0?
+		query = """
+				MATCH (e1:Employee)-->(c1:Company), (e2:Employee {eid: e1.eid})-->(c2:Company) 
+				WHERE e1.edate < e2.sdate
+				MERGE (c1)-[t: TRANS {weight:0}]->(c2)
+			"""
+
+		g.run(query)
+
+		# add weight to the edge
+		query = """
+				MATCH (e1:Employee)-->(c1:Company)-[T:TRANS]->(c2:Company)<--(e2:Employee {eid: e1.eid})
+				WHERE e1.edate < e2.sdate
+				SET T.weight = T.weight + 1
+			"""
+
+		g.run(query)
+
+		# decrement number of employees of companies after transition happens
+		query = """
+				MATCH (e1:Employee)-->(c1:Company)-[T:TRANS]->(c2:Company)<--(e2:Employee {eid: e1.eid})
+				WHERE e1.edate  < e2.sdate
+				SET c1.totalNumber = c1.totalNumber - 1
+			"""
+
+		g.run(query)
+
+		# decrement number of elite employees of companies after transition happens
+		query = """
+				MATCH (e1:Employee)-->(c1:Company)-[T:TRANS]->(c2:Company)<--(e2:Employee {eid: e1.eid})
+				WHERE e1.edate  < e2.sdate AND e1.elite CONTAINS "True"
+				SET c1.eliteNumber = c1.eliteNumber - 1
+			"""
+
+		g.run(query)
+	# run page rank & output the output of page rank as csv file
+	query = """
+			CALL algo.pageRank.stream('Company', 'TRANS', {
+  			iterations:20, dampingFactor:0.85, weightProperty: "weight"})
+			YIELD nodeId, score
+			RETURN algo.asNode(nodeId).name AS page,score
+			ORDER BY score DESC
+	"""
+	res = g.run(query).to_data_frame()
+	res.to_csv(output_path + str(year) + 'pagerank.csv')
+
+
+# process through all years & output a page rank per year
+batch_size = 100000
+# customize the output file
+output_path = argv[1]
+years = range(2000, 2015)
+for year in years:
+	process_one_year(year, batch_size, output_path)
