86dc0dc64d9070fdfca91feda72b5e7c89a7f17c
Steven Deng <hongdeng@berkeley.edu>
2019-12-28 10:21:58 +0800

    Revised transition edge weights & allow customized input location

--- company_rank/final.py
+++ company_rank/final.py
@@ -11,17 +11,12 @@ password = "123456"
 # initialize the graph object
 g = Graph(uri=uri, user=user, password=password)
 
-# prepare paths to desired datasets
-dataset_names = [x for x in os.listdir('../cognism-enriched/') if 'done' not in x]
-
-
-
 # process one year of data and produce a page rank by iterating through all datasets for one year
-def process_one_year(year, batch_size, output_path):
+def process_one_year(year, batch_size, dataset_names, output_path):
 	# load the yearly graph
 	for dataset in dataset_names:
 		# absolute path of the dataset
-		file_path = 'file:///DATA-0/fedyk/cognism-enriched/' + dataset
+		file_path = dataset
 		# loading & graph building 
 		query = """
 				CALL apoc.load.json({}) YIELD value 
@@ -49,11 +44,11 @@ def process_one_year(year, batch_size, output_path):
 
 		g.run(query)
 
-		# if an egde already exits, will this set the weight of already existing edge to 0?
+		# if an edge already exits, will this set the weight of already existing edge to 0?
 		query = """
 				MATCH (e1:Employee)-->(c1:Company), (e2:Employee {eid: e1.eid})-->(c2:Company) 
 				WHERE e1.edate < e2.sdate
-				MERGE (c1)-[t: TRANS {weight:0}]->(c2)
+				CREATE UNIQUE (c1)-[t: TRANS {weight:0}]->(c2)
 			"""
 
 		g.run(query)
@@ -61,9 +56,9 @@ def process_one_year(year, batch_size, output_path):
 		# add weight to the edge
 		query = """
 				MATCH (e1:Employee)-->(c1:Company)-[T:TRANS]->(c2:Company)<--(e2:Employee {eid: e1.eid})
-				WHERE e1.edate < e2.sdate
+				WHERE e1.edate < e2.sdate AND e1.edate CONTAINS {} OR e2.sdate CONTAINS {}
 				SET T.weight = T.weight + 1
-			"""
+			""".format('\'' + str(year) + '\'', '\'' + str(year) + '\'')
 
 		g.run(query)
 
@@ -95,11 +90,20 @@ def process_one_year(year, batch_size, output_path):
 	res = g.run(query).to_data_frame()
 	res.to_csv(output_path + str(year) + 'pagerank.csv')
 
+	# delete all edges(transition) in this year so it won't mess up with edges in next year
+	query = """
+			MATCH ()-[t]->() DELETE t
+	"""
+	g.run(query)
 
 # process through all years & output a page rank per year
 batch_size = 100000
+# customize input location
+input_path = argv[1]
 # customize the output file
-output_path = argv[1]
+output_path = argv[2]
+# prepare paths to desired datasets
+dataset_names = [x for x in os.listdir(input_path) if 'done' not in x]
 years = range(2000, 2015)
 for year in years:
-	process_one_year(year, batch_size, output_path)
+	process_one_year(year, batch_size, dataset_names, output_path)
